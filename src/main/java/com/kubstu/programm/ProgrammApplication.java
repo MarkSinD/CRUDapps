b40dc2b2e64d3df5e0b6990ed9209bc7eed115a2
Mark Sinakaev <high-school@bk.ru>
2021-06-01 11:55:07 +0300

    default clean project

---
+++ src/main/java/com/kubstu/programm/ProgrammApplication.java
@@ -0,0 +1,13 @@
+package com.kubstu.programm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProgrammApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kubstu/programm/ProgrammApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubstu.programm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
